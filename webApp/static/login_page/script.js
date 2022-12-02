29a81f30a96dcda9748a4d904f1a831c3dafb926
Candan <evan.candan@epitech.eu>
2022-12-02 03:30:22 +0100

    [+] add static pages

--- webApp/static/login_page/script.js
+++ webApp/static/login_page/script.js
@@ -1,3 +1,14 @@
 function make_req() {
-    
+    const email = document.getElementById("email_r");
+    const passwr = document.getElementById("pass_r");
+    const confirm_pass = document.getElementById("confirm_pass");
+    $.ajax({
+        type: "POST",
+        url: "/request",
+        data: "",
+        dataType: "dataType",
+        success: function (response) {
+            
+        }
+    });
 }
